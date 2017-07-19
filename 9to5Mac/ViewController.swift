b4a2d6928d040158d2765e084150bec838fb9150
Tim Roesner <timroesner@me.com>
2017-07-18 19:43:18 -0700

    Initial Commit

---
+++ 9to5Mac/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  9to5Mac
+//
+//  Created by Tim Roesner on 7/18/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
